7d5dfbcc689da75d72856a0f9111256db060200f
FORNER Yann <yann.forner@etu.univ-amu.fr>
2020-03-12 13:30:53 +0100

    initialize

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var mustache = require('mustache-express');
+
+var app = express();
+
+app.use(express.urlencoded());
+
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+app.listen(3000, () => console.log('listening on http://localhost:3000'));
+
