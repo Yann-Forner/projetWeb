91623c9aaf73f39c2f192b3d4d756fcbdc3f78e0
jeremy.gau <caliban3010@gmail.com>
2020-03-16 20:39:44 +0100

    Création de la database

--- create_db.js
+++ create_db.js
@@ -13,11 +13,13 @@ const Sqlite = require('better-sqlite3');
 
 let db = new Sqlite('db.sqlite');
 var create_db =function () {
-    db.prepare('DROP TABLE IF EXISTS probleme').run();
+    db.prepare('DROP TABLE IF EXISTS user').run();
+    db.prepare('DROP TABLE IF EXISTS object').run();
+    db.prepare('DROP TABLE IF EXISTS exchange').run();
 
-    db.prepare('CREATE TABLE probleme (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT)').run();
-
-    db.prepare('INSERT INTO probleme VALUES (null,@nom)').run({nom : 'Test'});
+    db.prepare('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, surname TEXT, city TEXT, mail TEXT, phone TEXT)').run();
+    db.prepare('CREATE TABLE object (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, category TEXT)').run();
+    db.prepare('CREATE TABLE exchange (idUser INTEGER REFERENCES user.id, idObject INTEGER REFERENCES object.id, type TEXT)').run();
 };
 
 create_db();
