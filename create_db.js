0581f506702ac3ee556748fc1d61bbbdc0bab754
jeremy.gau <caliban3010@gmail.com>
2020-03-22 17:54:39 +0100

    changement contrainte table exchange

--- create_db.js
+++ create_db.js
@@ -17,9 +17,9 @@ var create_db =function () {
     db.prepare('DROP TABLE IF EXISTS object').run();
     db.prepare('DROP TABLE IF EXISTS user').run();
 
-    db.prepare('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, password TEXT, name TEXT, surname TEXT, city TEXT, mail TEXT, phone TEXT, role TEXT)').run();
-    db.prepare('CREATE TABLE object (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, category TEXT)').run();
-    db.prepare('CREATE TABLE exchange (idUser INTEGER REFERENCES user(id), idObject INTEGER REFERENCES object(id), type TEXT, PRIMARY KEY (idUser, idObject))').run();
+    db.prepare('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, password TEXT NOT NULL, name TEXT NOT NULL, surname TEXT NOT NULL, city TEXT NOT NULL, mail TEXT NOT NULL, phone TEXT, role TEXT NOT NULL)').run();
+    db.prepare('CREATE TABLE object (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, category TEXT NOT NULL)').run();
+    db.prepare('CREATE TABLE exchange (idUser INTEGER REFERENCES user(id), idObject INTEGER REFERENCES object(id), type TEXT NOT NULL, PRIMARY KEY (idUser, idObject))').run();
 
     db.prepare('INSERT INTO user VALUES (@id, @password, @name, @surname, @city, @mail, @phone, @role)')
         .run({id: null, password: 'root', name: 'admin', surname: 'admin', city: 'Marseille', mail: 'admin@admin.fr', phone: '0000000000', role: 'admin'})
