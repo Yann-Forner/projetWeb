e724ece79172e1a55e42e5fd00c6496d7dc84092
jeremy.gau <caliban3010@gmail.com>
2020-03-22 17:50:17 +0100

    changement contrainte table exchange

--- create_db.js
+++ create_db.js
@@ -19,7 +19,7 @@ var create_db =function () {
 
     db.prepare('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, password TEXT, name TEXT, surname TEXT, city TEXT, mail TEXT, phone TEXT, role TEXT)').run();
     db.prepare('CREATE TABLE object (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, category TEXT)').run();
-    db.prepare('CREATE TABLE exchange (idUser INTEGER REFERENCES user(id), idObject INTEGER REFERENCES object(id), type TEXT, PRIMARY KEY (idUser, idObject, type))').run();
+    db.prepare('CREATE TABLE exchange (idUser INTEGER REFERENCES user(id), idObject INTEGER REFERENCES object(id), type TEXT, PRIMARY KEY (idUser, idObject))').run();
 
     db.prepare('INSERT INTO user VALUES (@id, @password, @name, @surname, @city, @mail, @phone, @role)')
         .run({id: null, password: 'root', name: 'admin', surname: 'admin', city: 'Marseille', mail: 'admin@admin.fr', phone: '0000000000', role: 'admin'})
