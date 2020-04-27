f410e71f2821ea6e9121ff9907d2c2d86031e204
Yann <yann.forner@etu.univ-amu.fr>
2020-04-27 14:59:12 +0200

    boutton afficher/cacher mdp

--- js/script.js
+++ js/script.js
@@ -8,6 +8,17 @@ $( document ).ready(function() {
         $('#myType').val('surplus');
         $('#titleModalAddExchange').text('Ajout d\'un surplus');
     });
-
+    $('#buttonPwd').click(function () {
+        let myEntry = $('#password');
+        if(myEntry.prop('type') === 'password'){
+            myEntry.prop('type','text');
+            $(this).text('Cacher')
+        }
+        else{
+            myEntry.prop('type','password');
+            $(this).text('Afficher')
+        }
+    }
+    );
 });
 
